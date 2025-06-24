f837a01d6d1680c5bf76136d7f2ed85a0f846ee7
mahmoudfaleh <153992575+mahmoudfaleh@users.noreply.github.com>
2025-06-24 13:14:17 +0300

    dsasd

--- game/mobile/index.service.worker.js
+++ game/mobile/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750759575|8196040148';
+const CACHE_VERSION = '1750760051|8671707353';
 /** @type {string} */
 const CACHE_PREFIX = 'First Gameing-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
