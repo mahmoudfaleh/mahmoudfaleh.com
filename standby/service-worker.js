bb4e368e75cdf47e64994d9c6ce55d80dc427c2d
mahmoudfaleh <153992575+mahmoudfaleh@users.noreply.github.com>
2025-07-04 13:20:46 +0300

    Create service-worker.js

---
+++ standby/service-worker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'standby-cache-v1';
+const urlsToCache = [
+    '/standby/', // This is the start URL defined in your manifest
+    '/standby/index.html',
+    '/standby/photos.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+    // Paths for icons, assuming they are in a subfolder 'icons' within 'standby'
+    '/standby/icons/icon-192x192.png',
+    '/standby/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    (response) => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and can only be consumed once. We must clone it so that
+                        // we can consume the stream and the browser can consume it too.
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
